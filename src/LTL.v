50ce650f6f3cceaa9a6ed5ebeb7654be7a0810d1
John Wiegley <johnw@newartisans.com>
2019-04-12 12:53:33 -0700

    Fix a typo

--- src/LTL.v
+++ src/LTL.v
@@ -243,17 +243,14 @@ Ltac ltl :=
 
 (* These properties are proven to hold in up to three conditions:
 
-   ≈ (Weak)
-     The property holds, even if early terminate of the finite input causes a
-     match to be incomplete. For example, a proposition against an empty
-     input.
+   ≈ (Weak) The property holds, even if early termination of a finite input
+     causes the match to be incomplete. For example, a proposition against an
+     empty input.
 
-   ≈[strong]
-     The property holds only if it matches some infix of the finite input
-     exactly. Partial matches are not allowed.
+   ≈[strong] The property holds only if it matches some infix of the finite
+     input exactly. Partial matches are not allowed.
 
-   ≈[infinite]
-     The property holds only against infinite inputs. *)
+   ≈[infinite] The property holds only against infinite inputs. *)
 
 (* eventually ψ becomes true *)
 Lemma eventually_until (ψ : LTL) : ◇ ψ ≈ ⊤ U ψ.
