9234ad0b0718bd917b7abbe687829030298aab92
John Wiegley <johnw@newartisans.com>
2019-04-25 17:41:53 -0500

    Proof work in Step.v

--- src/Step.v
+++ src/Step.v
@@ -25,40 +25,16 @@ Variable a : Type.              (* The type of entries in the trace *)
 
 Inductive Failed : Type :=
   | HitBottom
+  | EndOfTrace
   | Rejected    (x : a)
   | BothFailed  (p q : Failed)
   | LeftFailed  (p : Failed)
   | RightFailed (q : Failed).
 
-Section Eff.
-  Inductive Eff (T : Type) : Type :=
-    | Stop   : T -> Eff T
-    | Delay  : Eff T -> Eff T
-    | Until  : Eff T -> Eff T -> Eff T -> Eff T
-    | Ask    : (a -> Eff T) -> Eff T.
-
-(*
-  Context {A B} (f : A -> B).
-
-  Fixpoint fmap' (m : Eff A) : Eff B :=
-    match m with
-    | Stop _ x  => Stop _ (f x)
-    | Delay _ m => Delay _ (fmap' m)
-    | Repeat _  => Repeat _
-    | Ask _ k   => Ask _ (fmap' ∘ k)
-    end.
-*)
-End Eff.
-
-Arguments Stop {T} _.
-Arguments Delay {T} _.
-Arguments Until {T} _ _ _.
-Arguments Ask {T} _.
-
-(* Definition fmap {A B} := @fmap' A B. *)
-
 Inductive Result : Type :=
+  (* | Ask (f : a -> Result) *)
   | Failure (e : Failed)
+  | Continue (l : LTL a)
   | Success.
 
 (*
@@ -78,89 +54,314 @@ Open Scope ltl_scope.
 
 Arguments expand {_} _.
 
-Definition Machine := Eff Result.
-
-Lemma and_until (ρ φ ψ τ : LTL a) : (φ ∧ ψ) U ρ ≈ (φ U ρ) ∧ (ψ U τ).
-Proof.
-  symmetry.
-  rewrite expand_until.
-  rewrite (expand_until _ ψ τ).
-  rewrite !and_or.
-
-(* Implements "and" behavior *)
-Fixpoint combine (f g : Eff Result) : Eff Result :=
+Definition and (f g : Result) : Result :=
   match f, g with
-  | Stop (Failure e), _     => Stop (Failure (LeftFailed e))
-  | _, Stop (Failure e)     => Stop (Failure (RightFailed e))
-  | Delay f', Delay g'      => Delay (combine f' g')
-  | Delay f', Until q' p' r => Until (combine (Delay f') q') (combine (Delay f') p') r
-  | Until q' p' r, Delay g' => Until (combine q' (Delay g')) (combine p' (Delay g')) r
-  | Until q1' p1' r1,
-    Until q2' p2' r2        => Until (combine q1' q2') (combine p1' p2') (combine r1 r2)
-  | Ask f', Ask g'          => Ask (fun a => combine (f' a) (g' a))
-  | Delay f', Ask g'        => Ask (fun a => combine (Delay f') (g' a))
-  | Ask f', Delay g'        => Ask (fun a => combine (f' a) (Delay g'))
-  | f', Stop Success        => f'
-  | Stop Success, g'        => g'
+  | Failure e, _   => Failure (LeftFailed e)
+  | _, Failure e   => Failure (RightFailed e)
+  | Continue f',
+    Continue g'    => Continue (f' ∧ g')
+  | f', Success    => f'
+  | Success, g'    => g'
+  (* | Ask f', Ask g' => Ask (fun a => and (f' a) (g' a)) *)
+  (* | f', Ask g'     => Ask (fun a => and f' (g' a)) *)
+  (* | Ask f', g'     => Ask (fun a => and (f' a) g') *)
   end.
 
-(* Implements "or" behavior *)
-Fixpoint select (f g : Eff Result) : Eff Result :=
+Definition or (f g : Result) : Result :=
   match f, g with
-  | Stop (Failure e1),
-    Stop (Failure e2)    => Stop (Failure (BothFailed e1 e2))
-  | Stop Success, _      => Stop Success
-  | _, Stop Success      => Stop Success
-  | Delay f', Delay g'   => Delay (select f' g')
-  | Ask f', Ask g'       => Ask (fun a => selectQuery (f' a) (g' a))
-  | Delay f', Ask g'     => Ask (fun a => select (Delay f') (g' a))
-  | Ask f', Delay g'     => Ask (fun a => select (f' a) (Delay g'))
-  | Stop (Failure _), g' => g'
-  | f', Stop (Failure _) => f'
+  | Failure e1,
+    Failure e2     => Failure (BothFailed e1 e2)
+  | Success, _     => Success
+  | _, Success     => Success
+  | Continue f',
+    Continue g'    => Continue (f' ∨ g')
+  | Failure _, g'  => g'
+  | f', Failure _  => f'
+  (* | Ask f', Ask g' => Ask (fun a => and (f' a) (g' a)) *)
+  (* | f', Ask g'     => Ask (fun a => and f' (g' a)) *)
+  (* | Ask f', g'     => Ask (fun a => and (f' a) g') *)
   end.
 
-Fixpoint step (m : Machine) (x : a) : Machine :=
-  match m with
-  | Stop x    => Stop x
-  | Delay m   => m
-  | Until p q => select (step q x) (combine (step p x) (Delay m))
-  | Ask k     => step (k x) x
-  end.
+Definition Machine := option a -> Result.
 
-Definition run (m : Machine) (xs : list a) : Machine :=
-  fold_left step xs m.
-
-Fixpoint compile (l : LTL a) : Machine :=
+Fixpoint compile (l : LTL a) : Machine := fun mx =>
   match l with
-  | ⊤ => Ready (Stop Success)
-  | ⊥ => Ready (Stop (Failure HitBottom))
+  | ⊤ => Success
+  | ⊥ => Failure HitBottom
 
-  | Accept v => Waiting (Ask (fun a => resolve (compile (v a)) a))
+  | Accept v =>
+    match mx with
+    | None   => Failure EndOfTrace
+    | Some x => compile (v x) (Some x)
+    end
   | Reject v =>
-    Waiting (Ask (fun x =>
-                    fmap (fun m =>
-                            match m with
-                            | Failure _ => Success
-                            | Success     => Failure (Rejected x)
-                            end)
-                         (resolve (compile (v x)) x)))
+    match mx with
+    | None   => Success
+    | Some x =>
+      match compile (v x) (Some x) with
+      | Failure _  => Success
+      | Success    => Failure (Rejected x)
+      | Continue p => Continue (negate _ p)
+      end
+    end
 
-  | p ∧ q => combine (compile p) (compile q)
-  | p ∨ q => select (compile p) (compile q)
+  | p ∧ q => and (compile p mx) (compile q mx)
+  | p ∨ q => or  (compile p mx) (compile q mx)
 
-  | X p   => Ready (defer (compile p))
+  | X p   =>
+    match mx with
+    | None   => Failure EndOfTrace
+    | Some _ => Continue p
+    end
 
-  | p U q => select (compile q) (combine (compile p) (Ready (defer (compile (p U q)))))
-  | p R q => combine (compile q) (select (compile p) (Ready (defer (compile (p R q)))))
+  | p U q =>
+    match mx with
+    | None   => compile q None
+    | Some x => or (compile q mx) (and (compile p mx) (Continue (p U q)))
+    end
+
+  | p R q =>
+    match mx with
+    | None   => and (compile q None) (compile p None)
+    | Some x => and (compile q mx) (or (compile p mx) (Continue (p R q)))
+    end
   end.
+
+Definition step (m : Result) (x : a) : Result :=
+  match m with
+  | Continue l => compile l (Some x)
+  | r => r
+  end.
+
+Definition and_then (r : Result) (s : LTL a -> Result) : Result :=
+  match r with
+  | Continue l => s l
+  | r => r
+  end.
+
+Fixpoint run (m : LTL a) (xs : list a) : Result :=
+  match xs with
+  | []      => compile m None
+  | x :: xs => and_then (compile m (Some x)) (fun l => run l xs)
+  end.
+
+Definition passes (r : Result) : Prop :=
+  match r with
+  | Failure e  => False
+  | Continue _ => True
+  | Success    => True
+  end.
+
+Definition passes_equiv (p q : Result) : Prop :=
+  passes p <-> passes q.
+Arguments passes_equiv p q /.
+
+Global Program Instance Equivalence_passes_equiv : Equivalence passes_equiv.
+Next Obligation.
+  unfold passes_equiv.
+  repeat intro; auto.
+  reflexivity.
+Qed.
+Next Obligation.
+  unfold passes_equiv.
+  repeat intro; intuition.
+Qed.
+Next Obligation.
+  unfold passes_equiv.
+  repeat intro; firstorder.
+Qed.
+
+Lemma run_and p q xs :
+  passes (run (p ∧ q) xs) <-> passes (run p xs) /\ passes (run q xs).
+Proof.
+  generalize dependent p.
+  generalize dependent q.
+  induction xs; simpl; intros;
+  intuition auto;
+  try destruct (compile p Nothing) eqn:?;
+  try destruct (compile q Nothing) eqn:?;
+  try destruct (compile p (Some a0)) eqn:?;
+  try destruct (compile q (Some a0)) eqn:?;
+  try specialize (proj1 (IHxs _ _) H); intros;
+  simpl in *; now intuition.
+Qed.
+
+Lemma run_reject p xs :
+  passes (run (¬ p) xs) <->
+  passes (match run p xs with
+          | Failure _  => Success
+          | Success    => Failure HitBottom
+          | Continue p => Continue (negate _ p)
+          end).
+Proof.
+  generalize dependent p.
+  induction xs; simpl; intros;
+  unfold and_then, Basics.compose;
+  intuition auto;
+  try destruct (compile (p a0) Nothing) eqn:?;
+  try destruct (compile (p a0) (Just a0)) eqn:?;
+  try specialize (proj1 (IHxs _ _) H); intros;
+  intuition.
+  - induction p; intuition.
+  - admit.
+  - admit.
+  -
+Abort.
+
+Lemma run_or p q xs :
+  passes (run (p ∨ q) xs) <-> passes (run p xs) \/ passes (run q xs).
+Proof.
+  generalize dependent p.
+  generalize dependent q.
+  induction xs; simpl; intros;
+  intuition auto;
+  try destruct (compile p Nothing) eqn:?;
+  try destruct (compile q Nothing) eqn:?;
+  try destruct (compile p (Some a0)) eqn:?;
+  try destruct (compile q (Some a0)) eqn:?;
+  try specialize (proj1 (IHxs _ _) H); intros;
+  simpl in *; now intuition.
+Qed.
+
+Lemma run_next p x xs :
+  passes (run (X p) (x :: xs)) <-> passes (run p xs).
+Proof. now induction xs; intuition. Qed.
+
+Lemma run_until p q s :
+  passes (run (p U q) s) <->
+  let fix go s :=
+      match s with
+      | [] => passes (run q s)
+      | _ :: xs => passes (run q s) \/ (passes (run p s) /\ go xs)
+      end in go s.
+Proof.
+  generalize dependent p.
+  generalize dependent q.
+  induction s; simpl; intros.
+  - split; auto.
+  - destruct (compile p (Some a0)) eqn:?;
+    destruct (compile q (Some a0)) eqn:?; simpl;
+    intuition auto.
+    + apply run_and in H.
+      intuition.
+    + apply IHs in H1.
+      apply run_and; auto.
+    + apply run_or in H.
+      destruct H; auto.
+      apply run_and in H.
+      intuition.
+    + apply run_or.
+      intuition.
+    + apply IHs in H1.
+      apply run_or; intuition.
+      right.
+      apply run_and; auto.
+    + right.
+      split; auto.
+      now apply IHs.
+    + now apply IHs in H1.
+    + apply run_or in H.
+      intuition.
+    + apply run_or.
+      intuition.
+    + apply IHs in H1.
+      apply run_or.
+      intuition.
+Qed.
+
+Lemma run_release p q s :
+  passes (run (p R q) s) <->
+  let fix go s :=
+      match s with
+      | [] => passes (run q s) /\ passes (run p s)
+      | _ :: xs => passes (run q s) /\ (passes (run p s) \/ go xs)
+      end in go s.
+Proof.
+  generalize dependent p.
+  generalize dependent q.
+  induction s; intros.
+  - split; intros.
+      now apply run_and.
+    pose (run_and q p []).
+    simpl in *; intuition.
+  - simpl; unfold and_then; split; intros;
+    destruct (compile p (Some a0)) eqn:?;
+    destruct (compile q (Some a0)) eqn:?; simpl in *;
+    intuition auto.
+    + apply run_and in H.
+      now intuition.
+    + apply run_and in H.
+      now intuition.
+    + apply IHs in H.
+      now right.
+    + apply run_and in H.
+      now intuition.
+    + apply run_and in H.
+      destruct H.
+      apply run_or in H0.
+      now intuition.
+    + apply run_or in H.
+      now intuition.
+    + apply run_and.
+      now intuition.
+    + now apply IHs.
+    + apply run_and.
+      split; auto.
+      apply run_or.
+      now intuition.
+    + apply run_and.
+      split; auto.
+      apply run_or.
+      now intuition.
+    + apply run_or.
+      now intuition.
+    + apply run_or.
+      now intuition.
+Qed.
 
 Lemma run_correct (l : LTL a) (s : Stream a) :
-  (* Because matches may be partial on finite input, we only ensure that a
-     non-failing match is never a failure, and vice versa. *)
-  matches a l s
-    <-> forall e, run (compile l) s <> Ready (Stop (Failure e)).
+  matches a l s <-> passes (run l s).
 Proof.
-Abort.
+  generalize dependent s.
+  induction l; simpl; intros.
+  - now induction s.
+  - now induction s.
+  - induction s; intuition.
+    now specialize (proj1 (H _ _) H2).
+  - induction s.
+      now intuition.
+    admit.
+  - split; intros.
+      destruct H.
+      specialize (proj1 (IHl1 _) H); intros; clear IHl1 H.
+      specialize (proj1 (IHl2 _) H0); intros; clear IHl2 H0.
+      now apply run_and.
+    apply run_and in H.
+    now split; intuition.
+  - split; intros.
+      destruct H.
+        specialize (proj1 (IHl1 _) H); intros; clear IHl1 H.
+        apply run_or.
+        now left.
+      specialize (proj1 (IHl2 _) H); intros; clear IHl2 H.
+      apply run_or.
+      now right.
+    apply run_or in H.
+    now intuition.
+  - now induction s; intuition.
+  - split; intros.
+      apply run_until.
+      simpl.
+      induction s; intuition.
+    apply run_until in H.
+    induction s; intuition.
+  - split; intros.
+      apply run_release.
+      simpl.
+      induction s; intuition.
+    apply run_release in H.
+    induction s; intuition.
+Admitted.
 
 End Step.
 
