dd0914969553d1ade98e0d5c6fb53592e5651401
John Wiegley <johnw@newartisans.com>
2024-09-07 09:54:21 -0700

    Change to using a Nix flake

--- default.nix
+++ default.nix
@@ -1,42 +1,7 @@
-args@{
-  rev    ? "07262b18b97000d16a4bdb003418bd2fb067a932"
-, sha256 ? "05rnlf6vvzvyhya48bql9j0b6rqmiajzx0yasal522pwi1g2m122"
-, pkgs   ? import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    inherit sha256; }) {
-    config.allowUnfree = true;
-    config.allowBroken = false;
-  }
-}:
-
-let coq-cds4ltl = coqPackages:
-  with pkgs.${coqPackages}; pkgs.stdenv.mkDerivation rec {
-    name = "coq${coq.coq-version}-cds4ltl-${version}";
-    version = "1.0";
-
-    src = if pkgs ? coqFilterSource
-          then pkgs.coqFilterSource [] ./.
-          else ./.;
-
-    buildInputs = [
-      coq coq.ocaml coq.camlp5 coq.findlib
-    ];
-    enableParallelBuilding = true;
-
-    installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
-
-    env = pkgs.buildEnv { inherit name; paths = buildInputs; };
-    passthru = {
-      compatibleCoqVersions = v:
-        builtins.elem v [ "8.14" "8.15" "8.16" "8.17" "8.18" "8.19" ];
-    };
-  };
-
-in {
-  coq-cds4ltl_8_14 = coq-cds4ltl "coqPackages_8_14";
-  coq-cds4ltl_8_15 = coq-cds4ltl "coqPackages_8_15";
-  coq-cds4ltl_8_16 = coq-cds4ltl "coqPackages_8_16";
-  coq-cds4ltl_8_17 = coq-cds4ltl "coqPackages_8_17";
-  coq-cds4ltl_8_18 = coq-cds4ltl "coqPackages_8_18";
-  coq-cds4ltl_8_19 = coq-cds4ltl "coqPackages_8_19";
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz";
+    sha256 = "1prd9b1xx8c0sfwnyzkspplh30m613j42l1k789s521f4kv4c2z2"; }
+) {
+  src =  ./.;
+}).defaultNix
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Implementation in Coq of the axiomatic system for linear temporal logic CDS4LTL";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in rec {
+        packages = rec {
+          coq-cds4ltl = coqPackages: with pkgs.${coqPackages}; pkgs.stdenv.mkDerivation rec {
+            name = "coq${coq.coq-version}-coq-cds4ltl-${version}";
+            version = "1.0";
+
+            src = if pkgs ? coqFilterSource
+            then pkgs.coqFilterSource [] ./.
+            else ./.;
+
+            buildInputs = [
+              coq coq.ocaml coq.findlib
+            ] ++ pkgs.lib.optionals (coqPackages == "coqPackages_8_14" ||
+                                     coqPackages == "coqPackages_8_15") [
+              dpdgraph
+            ];
+            enableParallelBuilding = true;
+
+            configurePhase = "coq_makefile -f _CoqProject -o Makefile.coq";
+
+            installFlags = [
+              "COQLIB=$(out)/lib/coq/${coq.coq-version}/"
+              "COQLIBINSTALL=$(out)/lib/coq/${coq.coq-version}/user-contrib"
+              "COQPLUGININSTALL=$(OCAMLFIND_DESTDIR)"
+              "DOCDIR=$(out)/share/coq/${coq.coq-version}/"
+              "COQDOCINSTALL=$(out)/share/coq/${coq.coq-version}/user-contrib"
+            ];
+
+            env = pkgs.buildEnv { inherit name; paths = buildInputs; };
+            passthru = {
+              compatibleCoqVersions = v:
+              builtins.elem v [ "8.14" "8.15" "8.16" "8.17" "8.18" "8.19" ];
+            };
+          };
+
+          coq-cds4ltl_8_14 = coq-cds4ltl "coqPackages_8_14";
+          coq-cds4ltl_8_15 = coq-cds4ltl "coqPackages_8_15";
+          coq-cds4ltl_8_16 = coq-cds4ltl "coqPackages_8_16";
+          coq-cds4ltl_8_17 = coq-cds4ltl "coqPackages_8_17";
+          coq-cds4ltl_8_18 = coq-cds4ltl "coqPackages_8_18";
+          coq-cds4ltl_8_19 = coq-cds4ltl "coqPackages_8_19";
+
+          default = coq-cds4ltl_8_19;
+        };
+
+        defaultPackage = packages.default;
+      });
+}
